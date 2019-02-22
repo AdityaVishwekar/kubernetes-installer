60b6c911a39e1f990bd037411008f26d4c6f0617
osboxes.org <avishwekar10893@gmail.com>
2019-02-22 02:22:40 +0000

    update vars

--- source.sh
+++ source.sh
@@ -1,1 +1,4 @@
 export KUBECONFIG=/home/osboxes/terraform-kubernetes-installer/generated/kubeconfig
+alias etcdad2-0="ssh -i /home/osboxes/terraform-kubernetes-installer/generated/instances_id_rsa opc@"
+alias masterad1-0="ssh -i /home/osboxes/terraform-kubernetes-installer/generated/instances_id_rsa opc@"
+alias workerad3-0="ssh -i /home/osboxes/terraform-kubernetes-installer/generated/instances_id_rsa opc@"
